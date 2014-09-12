f11e95e8bb9ab384028a99c9c48d55ca63198586
Philipp Böcker <boecker@raumacht.de>
2014-09-12 05:02:48 +0200

    Initial Commit

--- laravel/app/views/default.php
+++ laravel/app/views/default.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
+
 <html>
+
 	<head>
 
 		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
@@ -11,40 +13,42 @@
 		<meta name="MobileOptimized" content="320" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-		<link href="css/frontend.css" rel="stylesheet" />
+		<link href="css/frontend.min.css" rel="stylesheet" />
 
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.JS"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.JS"></script>
 	<![endif]-->
 
 	</head>
 
-	<body id="top">
+	<body>
 
 		<header id="header" role="banner">
 			<div class="navbar navbar-default navbar-fixed-top">
 				<div class="container">
-					<div class="navbar-header">
-						<header>
-							<hgroup>
-								<h1>The Task for the 2nd Meeting/</h1>
-								<h2>Site Explore</h2>
-							</hgroup>
-						</header>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="navbar-header">
+							<header>
+								<hgroup>
+									<h1>The Task for the 2nd Meeting/</h1>
+									<h2>Site Explore</h2>
+								</hgroup>
+							</header>
+						</div>
 					</div>
+				</div>
 				</div>
 			</div>
 		</header>
 
 		<main id="main" role="main" class="container">
 			<div class="row">
-
 				<section class="col-sm-4">
 					<header>
 						<h1>engelhorn</h1>
 					</header>
-
 					<figure>
 						<a href="img/brand/engelhorn.svg" data-toggle="lightbox" data-width="640" data-footer="Brand: engelhorn">
 							<img class="img-responsive" alt="Brand: engelhorn" src="img/brand/engelhorn.svg">
@@ -53,7 +57,6 @@
 							<a href="http://www.engelhorn.de/fashion" title="Mode & Fashion online kaufen im engelhorn fashion e-shop" target="_blank">http://www.engelhorn.de/fashion</a>
 						</figcaption>
 					</figure>
-
 					<section>
 						<header>
 							<h1>At a glance</h1>
@@ -65,7 +68,6 @@
 							<dd>Demandware</dd>
 						</dl>
 					</section>
-
 					<section>
 						<header>
 							<h1>Settings</h1>
@@ -77,7 +79,6 @@
 							<dd class="icon good">HTML5</dd>
 						</dl>
 					</section>
-
 					<section>
 						<header>
 							<h1>Features</h1>
@@ -93,7 +94,6 @@
 							<dd>No</dd>
 						</dl>
 					</section>
-
 					<section>
 						<header>
 							<h1>Nerd Stuff</h1>
@@ -104,20 +104,17 @@
 							<dd>1.8 MB</dd>
 							<dd class="icon bad">9.09 s</dd>
 							<dt>Minification</dt>
-							<dd>css: No</dd>
-							<dd>js: No</dd>
+							<dd>CSS: No</dd>
+							<dd>JS: No</dd>
 							<dt>HTML5 Semantic</dt>
 							<dd>No</dd>
 						</dl>
 					</section>
-
 				</section>
-
 				<section class="col-sm-4">
 					<header>
 						<h1>Gerry Weber</h1>
 					</header>
-
 					<figure>
 						<a href="img/brand/gerry-weber.svg" data-toggle="lightbox" data-width="640" data-footer="Brand: Gerry Weber">
 							<img class="img-responsive" alt="Brand: Gerry Weber" src="img/brand/gerry-weber.svg">
@@ -126,7 +123,6 @@
 							<a href="http://www.house-of-gerryweber.de" title="Damenmode & Damenbekleidung im GERRY WEBER Online Shop bestellen" target="_blank">http://www.house-of-gerryweber.de</a>
 						</figcaption>
 					</figure>
-
 					<section>
 						<header>
 							<h1>At a glance</h1>
@@ -138,7 +134,6 @@
 							<dd>Demandware</dd>
 						</dl>
 					</section>
-
 					<section>
 						<header>
 							<h1>Settings</h1>
@@ -150,7 +145,6 @@
 							<dd>XHTML 1.0 Strict</dd>
 						</dl>
 					</section>
-
 					<section>
 						<header>
 							<h1>Features</h1>
@@ -166,7 +160,6 @@
 							<dd>No</dd>
 						</dl>
 					</section>
-
 					<section>
 						<header>
 							<h1>Nerd Stuff</h1>
@@ -177,20 +170,17 @@
 							<dd>583.8 KB</dd>
 							<dd class="icon good">2.04 s</dd>
 							<dt>Minification</dt>
-							<dd>css: No</dd>
-							<dd>js: No</dd>
+							<dd>CSS: No</dd>
+							<dd>JS: No</dd>
 							<dt>HTML5 Semantic</dt>
 							<dd>No</dd>
 						</dl>
 					</section>
-
 				</section>
-
 				<section class="col-sm-4">
 					<header>
 						<h1>s.Oliver</h1>
 					</header>
-
 					<figure>
 						<a href="img/brand/s-oliver.svg" data-toggle="lightbox" data-width="640" data-footer="Brand: s.Oliver">
 							<img class="img-responsive" alt="Brand: s.Oliver" src="img/brand/s-oliver.svg">
@@ -199,7 +189,6 @@
 							<a href="http://www.soliver.de/" title="Mode, Kleidung und Accessories im s.Oliver Online Shop kaufen" target="_blank">http://www.soliver.de</a>
 						</figcaption>
 					</figure>
-
 					<section>
 						<header>
 							<h1>At a glance</h1>
@@ -211,7 +200,6 @@
 							<dd>Demandware</dd>
 						</dl>
 					</section>
-
 					<section>
 						<header>
 							<h1>Settings</h1>
@@ -223,7 +211,6 @@
 							<dd class="icon bad">XHTML 1.0 Transitional</dd>
 						</dl>
 					</section>
-
 					<section>
 						<header>
 							<h1>Features</h1>
@@ -239,7 +226,6 @@
 							<dd class="icon good">Yes</dd>
 						</dl>
 					</section>
-
 					<section>
 						<header>
 							<h1>Nerd Stuff</h1>
@@ -250,22 +236,96 @@
 							<dd>1.7 MB</dd>
 							<dd>5.96 s</dd>
 							<dt>Minification</dt>
-							<dd>css: No</dd>
-							<dd>js: No</dd>
+							<dd>CSS: No</dd>
+							<dd>JS: No</dd>
 							<dt>HTML5 Semantic</dt>
 							<dd>No</dd>
 						</dl>
 					</section>
-
 				</section>
 			</div>
 		</main>
 
+		<footer id="footer" role="contentinfo">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<header>
+							<h1>Additional Site Informations</h1>
+						</header>
+							<section class="additional">
+								<header>
+									<h1>This Site is build by: Philipp Böcker</h1>
+								</header>
+								<dl>
+									<dt>Laravel</dt>
+									<dd>The PHP Framework For Web Artisans</dd>
+									<dt>Twitter Bootstrap (Less) + jQuery: The Write Less, Do More, JavaScript Library</dt>
+									<dd>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web</dd>
+									<dt>Node.JS</dt>
+									<dd>Node.JS is a platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications</dd>
+									<dt>Grunt</dt>
+									<dd>The JavaScript Task Runner</dd>
+									<dt>Bower</dt>
+									<dd>A package manager for the web</dd>
+									<dt>Bower Dependencies</dt>
+									<dd>Bootstrap Auto-Hiding Navbar</dd>
+									<dd>Font Awesome</dd>
+									<dd>Lightbox for Bootstrap 3</dd>
+									<dt>Composer</dt>
+									<dd>Dependency Manager for PHP</dd>
+								</dl>
+							</section>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<section class="additional">
+							<header>
+								<h1>Settings</h1>
+							</header>
+							<dl>
+								<dt>Encoding</dt>
+								<dd>utf-8</dd>
+								<dt>Doctype</dt>
+								<dd>HTML5</dd>
+							</dl>
+						</section>
+					</div>
+					<div class="col-sm-4">
+					<section class="additional">
+						<header>
+							<h1>Features</h1>
+						</header>
+						<dl>
+							<dt>Responsive</dt>
+							<dd>Desktop: Yes</dd>
+							<dd>Tablet: Yes</dd>
+							<dd>Mobile: Yes</dd>
+						</dl>
+					</section>
+					</div>
+					<div class="col-sm-4">
+						<section class="additional">
+							<header>
+								<h1>Nerd Stuff</h1>
+							</header>
+							<dl>
+								<dt>Minification</dt>
+								<dd>CSS / JS: Yes</dd>
+								<dt>HTML5 Semantic</dt>
+								<dd>Yes</dd>
+							</dl>
+						</section>
+					</div>
+				</div>
+			</div>
+		</footer>
 
-
-		<script src="js/frontend.js"></script>
+		<script src="js/frontend.min.js"></script>
 		<script type="text/javascript">$("div.navbar-fixed-top").autoHidingNavbar();</script>
 		<script type="text/javascript">$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) { event.preventDefault(); $(this).ekkoLightbox(); });</script>
 
 	</body>
+
 </html>
